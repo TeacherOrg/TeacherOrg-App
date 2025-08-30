eccac3579452e853c7269fd791edb695a296f498
Niclas Basler <deine.email@beispiel.de>
2025-08-30 14:31:11 +0200

    main fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/index.css';
-import { BrowserRouter } from 'react-router-dom';  // Neu: Import des BrowserRouters
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>  // Neu: Wrap um die App
-    <App />
-  </BrowserRouter>
+  <App />
 );
