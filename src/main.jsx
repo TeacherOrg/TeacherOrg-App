3cf222c3384a6b4261799202f3c2a5e91390aa2f
Niclas Basler <deine.email@beispiel.de>
2025-08-30 14:11:32 +0200

    main fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App.jsx';
+import '@/index.css';
+import { BrowserRouter } from 'react-router-dom';  // Neu: Import des BrowserRouters
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>  // Neu: Wrap um die App
     <App />
-) 
+  </BrowserRouter>
+);
